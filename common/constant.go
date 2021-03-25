bb54763f93f83590f3d240c57a2ef2d23bc6e451
Daren Liang <daren@darenliang.com>
2021-03-24 21:32:03 -0400

    Bump to 1.4.0

--- common/constant.go
+++ common/constant.go
@@ -2,7 +2,7 @@ package common
 
 const (
 	// Version is current dscli app version
-	Version = "1.3.3"
+	Version = "1.4.0"
 
 	// MaxDiscordFileSize represents the maximum file size in bytes that an
 	// attachment can be
